40f8110bf5f5764d8f615f60397b46a2cebf6971
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-10-24 11:35:46 +0200

    es util

--- es_util.py
+++ es_util.py
@@ -9,22 +9,26 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import expand_action, _process_bulk_chunk, _chunk_actions
 
 
-def build_es_action(datum, index_name, es_type, op_type='index'):
-    _source = {k: None if isinstance(v, str) and len(v) == 0 else v for k, v in datum.items()}
+def build_es_action(datum, index_name, es_type, op_type="index"):
+    _source = {
+        k: None if isinstance(v, str) and len(v) == 0 else v for k, v in datum.items()
+    }
     doc = {
-        '_id': hashlib.sha1(json.dumps(_source).encode('utf-8')).hexdigest(),
-        '_op_type': op_type,
-        '_index': index_name,
-        '_type': es_type,
-        '_source': _source
+        "_id": hashlib.sha1(json.dumps(_source).encode("utf-8")).hexdigest(),
+        "_op_type": op_type,
+        "_index": index_name,
+        "_type": es_type,
+        "_source": _source,
     }
     return doc
 
 
-def build_es_client(
-        host = 'localhost',port=9200,
-        timeout=30
-    ):
-    es = Elasticsearch(hosts=[{"host": host, "port": port}],timeout=timeout,max_retries=3, retry_on_timeout=True)
-    es.cluster.health(wait_for_status='yellow', request_timeout=1)
+def build_es_client(host="localhost", port=9200, timeout=30) -> Elasticsearch:
+    es = Elasticsearch(
+        hosts=[{"host": host, "port": port}],
+        timeout=timeout,
+        max_retries=3,
+        retry_on_timeout=True,
+    )
+    es.cluster.health(wait_for_status="yellow", request_timeout=1)
     return es
